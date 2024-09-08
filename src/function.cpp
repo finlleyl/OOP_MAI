25b51facae3446f9cab7d76f7b43f563e9a89d0f
finlleyl <ilya_chibugaev@mail.ru>
2024-09-08 17:02:37 +0300

    Added basic files

---
+++ src/function.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ilya_ on 08.09.2024.
+//
+
+#include "function.h"
---
+++ src/function.h
@@ -0,0 +1,16 @@
+//
+// Created by ilya_ on 08.09.2024.
+//
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+
+
+class function {
+
+};
+
+
+
+#endif //FUNCTION_H
