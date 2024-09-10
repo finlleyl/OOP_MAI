d96c3c74e3fe485ba9b46596975a598b1afd3110
finlleyl <ilya_chibugaev@mail.ru>
2024-09-10 10:38:10 +0300

    bug fix

--- src/function.cpp
+++ src/function.cpp
@@ -6,7 +6,7 @@
 std::string convertTo24H(int hour, int minute, const std::string &period) {
     if (period == "AM" && hour == 12) {
         hour = 0;
-    } else if (period == "PM" && hour != 12) {
+    } else if (period == "PM" && hour < 12) {
         hour += 12;
     }
 
